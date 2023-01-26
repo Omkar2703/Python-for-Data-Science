3b63316435184a4d0d174de558f356d13ffa6db0
Omkar_Patil <omkar.ptl556@gmail.com>
2023-01-26 16:05:29 +0530

    Update

---
+++ Numpy/Py4.py
@@ -0,0 +1,11 @@
+#Operations on Numpy arrays
+#Calculating Mean and median of given array
+import numpy as np
+
+Marks = np.array([100, 102, 103, 104, 105, 106])
+
+print("Mean Marks: ", np.mean(Marks))
+print("Median Marks: ", np.median(Marks))
+# Output
+# Mean Marks:  103.33333333333333
+# Median Marks:  103.5
