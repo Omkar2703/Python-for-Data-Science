70169ece91a390a26b1513308b750c57116a7fca
Omkar_Patil <omkar.ptl556@gmail.com>
2023-01-26 14:12:15 +0530

    DS

---
+++ Numpy/Py1.py
@@ -0,0 +1,5 @@
+#Accessing element from 1D array
+import numpy as np
+cars = np.array(['Ertiga', 'Fortuner', 'Nexon', 'Grand Vitara', 'Baleno'])
+print(cars[0])
+#In Numpy, both positive and negative indices can be used to access elements in the ndarray. Positive indices start from the beginning of the array, while negative indices start from the end of the array. Array indexing starts from 0 in positive indexing and from -1 in negative indexing.
