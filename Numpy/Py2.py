c13133040243883c2a2634327e85b05c1ddca071
Omkar_Patil <omkar.ptl556@gmail.com>
2023-01-26 14:33:17 +0530

    Py2

---
+++ Numpy/Py2.py
@@ -0,0 +1,11 @@
+#Creating 2D array
+
+import numpy as np
+
+
+car_name = ['Ertiga', 'Fortuner', 'Nexon', 'Hector', 'Scorpio', 'Endeveour']
+
+horse_power = [300, 600, 150, 400, 200, 900]
+
+Cars = np.array([car_name, horse_power])
+print(Cars)
