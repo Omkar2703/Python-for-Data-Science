c55d8a440627f77f25ab8b4a8901970eb2373a2a
Omkar_Patil <omkar.ptl556@gmail.com>
2023-01-26 14:36:42 +0530

    Update

--- Numpy/Py2.py
+++ Numpy/Py2.py
@@ -9,3 +9,7 @@ horse_power = [300, 600, 150, 400, 200, 900]
 
 Cars = np.array([car_name, horse_power])
 print(Cars)
+
+# Output
+# [['Ertiga' 'Fortuner' 'Nexon' 'Hector' 'Scorpio' 'Endeveour']
+#  ['300' '600' '150' '400' '200' '900']]
